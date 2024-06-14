02b00e95ceab2133fcb0ab745c4a2e25d39b92f7
Romain <rsire@tuta.io>
2024-06-14 22:32:17 +0900

    feat(landing): replace hexagonal clip path by round shape with borders

--- src/components/ui/landing/Hero/Hero.tsx
+++ src/components/ui/landing/Hero/Hero.tsx
@@ -4,15 +4,13 @@ import style from "./Hero.module.scss";
 export default function Hero() {
 	return (
 		<section className={style.hero}>
-			<div className={style.imgWrapper}>
-				<Image
-					src="/profile.jpeg"
-					alt="My own headshot smiling in front of a park background"
-					width={350}
-					height={350}
-					className={style.img}
-				/>
-			</div>
+			<Image
+				src="/profile.jpeg"
+				alt="My own headshot smiling in front of a park background"
+				width={350}
+				height={350}
+				className={style.img}
+			/>
 			<h1 className={style.title}>Romain Siré</h1>
 			<p>
 				I&apos;m a french <strong>freelance front-end developer</strong> for hire,
