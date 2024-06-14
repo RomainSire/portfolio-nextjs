001b0a40c46406ed04b593938482442e2f6323df
Romain <rsire@tuta.io>
2024-06-14 23:34:12 +0900

    feat(metadata): add meta data for social networks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,25 @@ export const metadata: Metadata = {
 	description:
 		"Welcome to the portfolio of Romain Siré, a freelance frontend developer specializing in Typescript, React, Next.Js, Astro, and SASS. Discover his professional journey, explore his work, read insightful blog posts, and get in touch for collaborations or discussions.",
 	metadataBase: new URL("https://romainsire.com"),
+	openGraph: {
+		title: "Romain Siré - Frontend Developer",
+		description:
+			"Welcome to the portfolio of Romain Siré, a freelance frontend developer specializing in Typescript, React, Next.Js, Astro, and SASS. Discover his professional journey, explore his work, read insightful blog posts, and get in touch for collaborations or discussions.",
+		url: "https://romainsire.com",
+		siteName: "Romain Siré - Frontend Developer",
+		images: [
+			{
+				url: "https://romainsire.com/opengraph-image.jpg", // Must be an absolute URL
+				width: 1200,
+				height: 630,
+			},
+		],
+		locale: "en_US",
+		type: "website",
+	},
+	twitter: {
+		card: "summary_large_image",
+	},
 };
 
 /**
